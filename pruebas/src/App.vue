37d19993e93df81a4601b88c0f2de09f2f03b047
NEBULA1978 <rpascualey@gmail.com>
2023-09-01 14:04:46 +0200

    Separo primer componete de App.vue

--- pruebas/src/App.vue
+++ pruebas/src/App.vue
@@ -80,7 +80,30 @@ import Objetos from './components/Objetos.vue'
 import VforVif from './components/VforVif.vue'
 import EjemploUno from './components/EjemploUno.vue'
 
-
+const name = 'Vue 3';
+const styleColor = "color: red;";
+const styleColor2 = "color: blue;";
+const active = true;
+const colors = ["blue", "red", "green"];
+const counter = 1;
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+const arrayFrutas2 = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+  },
+];
 
 const ejemplo2 = `
   <h2>Hola {{ name }}!</h2>
