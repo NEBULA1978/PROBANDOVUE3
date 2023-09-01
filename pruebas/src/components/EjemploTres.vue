785a85776697238394e6cb73defb67eb81a46d91
NEBULA1978 <rpascualey@gmail.com>
2023-09-01 14:07:50 +0200

    Separo primer componente de App.vue añado en 4 componentes restantes falta

--- pruebas/src/components/EjemploTres.vue
+++ pruebas/src/components/EjemploTres.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>
-  </div>
+  
 </template>
 <script setup>
-// # Agrega lógica del componente EjemploTres aquí
+// # Agrega lógica del componente EjemploUno aquí
+const name = 'Vue 3';
+const styleColor = "color: red;";
+const styleColor2 = "color: blue;";
+const active = true;
+const colors = ["blue", "red", "green"];
+const counter = 1;
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+const arrayFrutas2 = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+  },
+];
+
+const ejemplo3 = ``;
 </script>
 <style scoped>
-/* # Estilos específicos para el componente EjemploTres */
+/* # Estilos específicos para el componente EjemploUno */
 </style>
