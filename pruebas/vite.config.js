4e260f51292348048e652fbdaf6c1cd8d02204ed
NEBULA1978 <rpascualey@gmail.com>
2023-09-19 22:27:02 +0200

    Subiendo a github pages 2

--- pruebas/vite.config.js
+++ pruebas/vite.config.js
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/PROBANDOVUE3/', // Reemplaza con el nombre de tu repositorio
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000, // Cambia el puerto según tus preferencias
+  },
+  build: {
+    outDir: 'docs', // Carpeta de salida para GitHub Pages
+  },
+  // Otras configuraciones que desees agregar...
+});
